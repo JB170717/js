d9343c8389c7a90c4a8957408c77ba98bd4cbbaf
JB170717 <bum2473@naver.com>
2025-03-11 17:32:58 +0900

    3.11js

---
+++ 03엘리먼트선택/sidebar.js
@@ -0,0 +1,30 @@
+
+var menuBtn = document.querySelector(".menuBtn");
+var sidenav = document.querySelector(".sidenav");   
+
+menuBtn.addEventListener("click", function(){
+
+    var w =sidenav.style.width;
+
+    // if(w == "0px" || w == ""){
+    //     sidenav.style.width = "200px";
+    // }else{
+    //     sidenav.style.width = "0px";
+    // }
+
+    if(Window.innerWidth <= 767){ //모바일사이즈
+        if(w=="0px"||w==""){
+            sidenav.style.width="100%";
+        }else{
+            sidenav.style.width="0px";
+        }
+    }else{ //브라우저사이즈
+        if(w =="0px"){
+            sidenav.style.width="200px";
+        }else{
+            sidenav.style.width="0px"
+        }
+    }
+    
+});
+
